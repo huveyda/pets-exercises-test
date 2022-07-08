4fe3c2e09ed13165ab46e28aef5c5975c4db59b4
huveydatukle <huveydatukle@gmail.com>
2022-07-08 14:12:37 +0300

    finished testing exercise

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,9 +1,14 @@
-
+import "./Filter.css"
 
 const Filter = ({filters, setFilters}) => <div className="pet-filter-container">
     <div className="filter-container">
         <label htmlFor="favourite">Favourite</label>
-        <select name="favourite" id="favourite" className="form-select">
+        <select name="favourite" id="favourite" className="form-select" onChange={(e) => {
+            setFilters({
+                ...filters,
+                favoured: e.target.value,
+            })
+        }}>
             <option value="any">Any</option>
             <option value="favoured">Favoured</option>
             <option value="not favoured">Not Favoured</option>
--- src/components/Pets/__test__/Pets.test.js
+++ src/components/Pets/__test__/Pets.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import Pets from "../Pets";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
@@ -35,5 +35,31 @@ describe("Pets", () => {
         const cards = await screen.findAllByRole("article");
         userEvent.selectOptions(screen.getByLabelText(/gender/i), "female");
         expect(screen.getAllByRole("article")).toStrictEqual([cards[0], cards[2], cards[4]]);
+    });
+    test("should filter for favoured cats", async () => {
+        const cards = await screen.findAllByRole("article");
+        userEvent.click(within(cards[0]).getByRole("button"))
+        userEvent.click(within(cards[3]).getByRole("button"))
+        userEvent.selectOptions(screen.getByLabelText(/favourite/i), "favoured");
+        expect(screen.getAllByRole("article")).toStrictEqual([cards[0], cards[3]]);
+    });
+    test("should filter for not favoured cats", async () => {
+        const cards = await screen.findAllByRole("article");
+        userEvent.click(within(cards[0]).getByRole("button"))
+        userEvent.click(within(cards[3]).getByRole("button"))
+
+        userEvent.selectOptions(screen.getByLabelText(/favourite/i), "not favoured");
+        expect(screen.getAllByRole("article")).toStrictEqual([cards[1], cards[2], cards[4]]);
+    });
+    test("should filter for favoured male cats", async () => {
+        const cards = await screen.findAllByRole("article");
+        userEvent.click(within(cards[0]).getByRole("button"));
+        userEvent.click(within(cards[3]).getByRole("button"));
+
+        userEvent.selectOptions(screen.getByLabelText(/favourite/i), "favoured");
+
+        userEvent.selectOptions(screen.getByLabelText(/gender/i), "male");
+
+        expect(screen.getAllByRole("article")).toStrictEqual([cards[3]]);
     });
 })
